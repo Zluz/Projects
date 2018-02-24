599b420f918157346e570def404e65c40088a891
usr106 <usr106@10.0.2.15>
2018-02-24 06:13:02 -0500

    Minor updates demonstrating Nest support, adding project files

--- pr113 - Nest Devices Interface/src/jmr/pr113/NestDevicesInterface.java
+++ pr113 - Nest Devices Interface/src/jmr/pr113/NestDevicesInterface.java
@@ -1,5 +1,6 @@
 package jmr.pr113;
 
+import java.util.Date;
 import java.util.Set;
 
 import com.bwssystems.nest.controller.Home;
@@ -8,13 +9,15 @@ import com.bwssystems.nest.controller.NestSession;
 import com.bwssystems.nest.controller.Thermostat;
 import com.bwssystems.nest.protocol.status.DeviceDetail;
 import com.bwssystems.nest.protocol.status.SharedDetail;
+import com.bwssystems.nest.protocol.status.WhereDetail;
+import com.bwssystems.nest.protocol.status.WhereItem;
 
 import jmr.util.SUProperty;
 import jmr.util.SystemUtil;
 
 public class NestDevicesInterface {
 
-	public static void main( final String[] args ) {
+	public static void main( final String[] args ) throws InterruptedException {
 		
 		final String strUsername = 
 				SystemUtil.getProperty( SUProperty.NEST_USERNAME ); 
@@ -42,7 +45,7 @@ public class NestDevicesInterface {
 														// 62657
 
 		final Nest nest = new Nest(session);
-
+		
 		System.out.println( "Nest information" );
 		System.out.println( "\tNest.toString() = " + nest.toString() );
 
@@ -56,9 +59,21 @@ public class NestDevicesInterface {
 		// home name:  d7d17cb0-ad43-11e1-a9b6-1231381b6879
 		
 		final Home home = nest.getHome( strHomeName );
+		
+		final WhereDetail wheres = nest.getWhere( strHomeName );
+		System.out.println( "WhereDetail.getOriginalJSON() = " 
+						+ wheres.getOriginalJSON() );
+		
 		System.out.println( "Home information:" );
 		System.out.println( "\tHome.getName() = " + home.getName() );
 //		System.out.println( "\tHome.getDetail() = " + home.getDetail() );
+		
+		System.out.println( "Where information:" );
+		for ( final WhereItem where : wheres.getWheres() ) {
+			System.out.println( "\tWhereDetail.getWhereId() = " + where.getWhereId() );
+			System.out.println( "\tWhereDetail.getName() = " + where.getName() );
+		}
+		
 
 //		aHome.setAway(Boolean.FALSE); /* either TRUE or FALSE */
 
@@ -72,26 +87,47 @@ public class NestDevicesInterface {
 		// device name:  01AA02AB111205MR
 		
 		
-		final Thermostat thermostat = nest.getThermostat( strDevice );
-		
-		final SharedDetail shared = thermostat.getSharedDetail();
+		for ( int i=1; i<100; i++ ) {
 
-		System.out.println( "SharedDetail information" );
-		System.out.println( "SharedDetail.getCurrentTemperature() = " 
-								+ shared.getCurrentTemperature() );
-		System.out.println( "SharedDetail.getTargetTemperature() = " 
-								+ shared.getTargetTemperature() );
-		System.out.println( "SharedDetail.getHvacFanState() = " 
-								+ shared.getHvacFanState() );
+			System.out.println( "Updating Thermostat" );
+			final Thermostat thermostat = nest.getThermostat( strDevice );
+			
+			for ( int j=1; j<4; j++ ) {
+				System.out.println( "Updating SharedDetail and DeviceDetail" );
+				final SharedDetail shared = thermostat.getSharedDetail();
 
+				final DeviceDetail detail = thermostat.getDeviceDetail();
+
+				for ( int k=1; k<4; k++ ) {
+					System.out.println( "\tTime now: " + new Date().toString() );
+					System.out.println( "SharedDetail information" );
+					final Double lTemp = shared.getCurrentTemperature();
+					System.out.println( "\tgetCurrentTemperature() = " + lTemp + " C = " + (lTemp * (9/5) + 32) + " F" );
+	//				System.out.println( "\tgetTargetTemperature() = " + shared.getTargetTemperature() );
+	//				System.out.println( "\tgetHvacFanState() = " + shared.getHvacFanState() );
+	//				System.out.println( "\tgetAutoAway() = " + shared.getAutoAway() );
+					final Long lTimestamp = shared.get$timestamp();
+					System.out.println( "\tget$timestamp() = " + lTimestamp + " = " + new Date( lTimestamp ).toString() );
+	//				System.out.println( "\tgetAutoAwayLearning() = " + shared.getAutoAwayLearning() );
+	//				System.out.println( "\tgetCompressorLockoutTimeout() = " + shared.getCompressorLockoutTimeout() );
+					System.out.println( "\tJSON: " + shared.getOriginalJSON() );
+
+					System.out.println( "DeviceDetail information" );
+					System.out.println( "\tJSON: " + detail.getOriginalJSON() );
+
+					final int iMinutes = 100000;
+					Thread.sleep( 1000 * 60 * iMinutes );
+				}
+			}
+		}
 		
-		final DeviceDetail detail = thermostat.getDeviceDetail();
-		
-		System.out.println( "DeviceDetail information" );
-		System.out.println( "DeviceDetail.getCurrentHumidity() = " 
-								+ detail.getCurrentHumidity() );
-		System.out.println( "DeviceDetail.getTemperatureScale() = " 
-								+ detail.getTemperatureScale() );
+//		final DeviceDetail detail = thermostat.getDeviceDetail();
+//		
+//		System.out.println( "DeviceDetail information" );
+//		System.out.println( "DeviceDetail.getCurrentHumidity() = " 
+//								+ detail.getCurrentHumidity() );
+//		System.out.println( "DeviceDetail.getTemperatureScale() = " 
+//								+ detail.getTemperatureScale() );
 		
 //		Float targetTemp = new Float(25.2790765); /* always a float and in celsius */
 //		thermo1.setTargetTemperature(targetTemp);
