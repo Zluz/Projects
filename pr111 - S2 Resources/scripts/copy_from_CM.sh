7957d54489616fc1cf1d45070416a5537265e666
usr106 (S) <usr106@10.0.2.15>
2018-03-17 13:43:36 -0400

    Adding LXPanel config to S2 Resourcespt (non-blocking) key inputxitl.sh"

--- pr111 - S2 Resources/scripts/copy_from_CM.sh
+++ pr111 - S2 Resources/scripts/copy_from_CM.sh
@@ -1,2 +1,3 @@
 rsync -vcI /Share/Development/CM/Git_Projects__20180128/pr111\ -\ S2\ Resources/scripts/*  /Local/scripts/
+rsync -vcI /Share/Development/CM/Git_Projects__20180128/pr111\ -\ S2\ Resources/config/*  /Local/config/
 
--- pr111 - S2 Resources/scripts/copy_to_CM.sh
+++ pr111 - S2 Resources/scripts/copy_to_CM.sh
@@ -1,2 +1,3 @@
 rsync -vcI /Local/scripts/* /Share/Development/CM/Git_Projects__20180128/pr111\ -\ S2\ Resources/scripts/
+rsync -vcI /Local/config/* /Share/Development/CM/Git_Projects__20180128/pr111\ -\ S2\ Resources/config/
 
