fc05138dcda0fd85cb585008d702c77d40818088
usr106 (S) <usr106@10.0.2.15>
2018-03-25 04:22:07 -0400

    Adding JSON output to remote script (for play_vlc.sh)more reliable)
    Seems like changes in networking (drive mounting timing) and OS version
    and available services seemed to affect how the startup script worked.
    May have been responsible for some RPis no longer booting.
    Big pain. Just run a script on a cron job.

--- pr111 - S2 Resources/scripts/play_vlc.sh
+++ pr111 - S2 Resources/scripts/play_vlc.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo "#JSON  {\"caption\":\"Stopping audio\"}"
+
 /Local/scripts/stop_all.sh
 
 echo "Starting VLC playback.."
 
+echo "#JSON  {\"caption\":\"Activating player\"}"
+
 WID=`xdotool search --name "VLC media player"`
+
+if [[ "$WID" == "" ]]
+then
+	echo "#JSON  {\"caption\":\"VLC not found\",\"status\":\"error\",\"status\":\"done\"}"
+	exit 1
+fi
+
 xdotool windowfocus $WID
 sleep 1
 # xdotool key ctrl+shift+p
@@ -14,12 +25,16 @@ xdotool key space
 sleep 1
 xdotool key alt+L E O
 
+echo "#JSON  {\"caption\":\"Checking playback\"}"
+
 LSOF_TEST=`lsof /dev/snd/* 2>/dev/null | grep vlc | head -1`
 if [[ "$LSOF_TEST" == "" ]]
 then
 	echo "WARNING: VLC audio did not appear to start."
+	echo "#JSON  {\"caption\":\"Audio not detected\",\"status\":\"error\"}"
 else
 	echo "VLC audio detected:"
 	echo "    $LSOF_TEST"
+	echo "#JSON  {\"caption\":\"Audio detected\",\"status\":\"done\"}"
 fi
 
