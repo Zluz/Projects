069077996ede9823f4f2753cd292c090e2c679a1
Zluz (S) <jeff.rabenhorst@gmail.com>
2019-06-02 23:10:19 -0400

    Updated image comparison script

--- pr111 - S2 Resources/scripts/compare_images.sh
+++ pr111 - S2 Resources/scripts/compare_images.sh
@@ -1,17 +1,24 @@
+# see	http://www.imagemagick.org/Usage/compare/#statistics
 
 export LHS=$1
 export RHS=$2
 
-# export LHS="D035-v2__001-night.jpg"
-# export RHS="D035-v2__car-night.jpg"
-# export RHS="D035-v2__002-snow.jpg"
-
 # export OUT1=`/usr/bin/convert $LHS $RHS  -compose Difference -composite -colorspace gray -verbose info:`
 # export OUT2=`echo $OUT1 | sed -n '/statistics:/,/^  [^ ]/p'`
-export OUT1=`/usr/bin/convert $LHS $RHS  -compose Difference -composite -colorspace gray -verbose info: | sed -n '/statistics:/,/^  [^ ]/p' | grep mean`
-export OUT3=`echo $OUT1 | tr '(' '\n' | tr ')' ' ' | grep -v mean`
+# export OUT1=`/usr/bin/convert $LHS $RHS  -compose Difference -composite fuzz 10% -colorspace gray -verbose info: | sed -n '/statistics:/,/^  [^ ]/p' | grep mean`
+# export OUT1=`/usr/bin/convert $LHS $RHS  -compose  Difference  -composite -fuzz 5%  -verbose info: | sed -n '/statistics:/,/^  [^ ]/p' | grep mean`
+# export OUT1=`/usr/bin/compare -verbose -metric MAE  $LHS $RHS null:`
+export OUT1=`/usr/bin/compare -verbose -metric MAE  $LHS $RHS null: 2>&1`
+export OUT2=`echo $OUT1`
+export OUT3=`echo $OUT2 | tr ' ' '\n' | grep '(' | tail -1`
+export OUT4=`echo $OUT3 | tr '(' '\n' | tr ')' ' '`
 
-# echo $OUT1
 # echo $OUT2
-echo $OUT3
+#echo "result:"
+echo $OUT4
+#echo
+#echo "processing:"
+#echo $OUT3
+#echo $OUT2
+#echo $OUT1
 
