c95a0e8dd19fc7d95d2612290bb656f1814a0610
Zluz (S) <jeff.rabenhorst@gmail.com>
2019-06-03 01:55:42 -0400

    Fixed compare script when using a mask

--- pr111 - S2 Resources/scripts/compare_images.sh
+++ pr111 - S2 Resources/scripts/compare_images.sh
@@ -8,20 +8,15 @@ export MASK=$3
 
 if [ "$#" -eq 3 ] && [ -f $MASK ]; then
 
-	export LHS="mask_$1"
-	export RHS="mask_$2"
+	export LHS="$1_mask"
+	export RHS="$2_mask"
 
 	/usr/bin/composite $1 $MASK $MASK $LHS
 	/usr/bin/composite $2 $MASK $MASK $RHS
 fi
 
-
-# export OUT1=`/usr/bin/convert $LHS $RHS  -compose Difference -composite -colorspace gray -verbose info:`
-# export OUT2=`echo $OUT1 | sed -n '/statistics:/,/^  [^ ]/p'`
-# export OUT1=`/usr/bin/convert $LHS $RHS  -compose Difference -composite fuzz 10% -colorspace gray -verbose info: | sed -n '/statistics:/,/^  [^ ]/p' | grep mean`
-# export OUT1=`/usr/bin/convert $LHS $RHS  -compose  Difference  -composite -fuzz 5%  -verbose info: | sed -n '/statistics:/,/^  [^ ]/p' | grep mean`
-# export OUT1=`/usr/bin/compare -verbose -metric MAE  $LHS $RHS null:`
-export OUT1=`/usr/bin/compare -verbose -metric MAE  $LHS $RHS null: 2>&1`
+export CMD="/usr/bin/compare -verbose -metric MAE  $LHS $RHS null:"
+export OUT1=`$CMD 2>&1`
 export OUT2=`echo $OUT1`
 export OUT3=`echo $OUT2 | tr ' ' '\n' | grep '(' | tail -1`
 export OUT4=`echo $OUT3 | tr '(' '\n' | tr ')' ' '`
@@ -31,9 +26,6 @@ if [ "$#" -eq 3 ] && [ -f $MASK ]; then
 	/bin/rm $RHS
 fi
 
-
-# echo $OUT2
-#echo "result:"
 echo $OUT4
 #echo
 #echo "processing:"
@@ -41,3 +33,4 @@ echo $OUT4
 #echo $OUT2
 #echo $OUT1
 
+echo "Command:  $CMD"
