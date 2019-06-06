30fd4f0b67e1560a5ccab3564293f489da6bb803
Zluz (C) <jeff.rabenhorst@gmail.com>
2019-06-06 18:59:16 -0400

    Much improved VisionGUI

--- pr129 - GCP Vision GUI/src/jmr/pr129/VisionGUI.java
+++ pr129 - GCP Vision GUI/src/jmr/pr129/VisionGUI.java
@@ -14,14 +14,19 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
 import com.google.cloud.vision.v1.BoundingPoly;
 import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.LocalizedObjectAnnotation;
+import com.google.cloud.vision.v1.LocationInfo;
 import com.google.cloud.vision.v1.NormalizedVertex;
 import com.google.cloud.vision.v1.Vertex;
 
@@ -31,7 +36,10 @@ public class VisionGUI {
 
 	final static String FILENAME_IMAGE_SOURCE = 
 //			"S:/xfer/Vision testing/unsorted/capture_vid2 - yard sale 002.jpg";
-			"S:/xfer/Vision testing/D035-v2 - car - plate.jpg";
+//			"S:/xfer/Vision testing/unsorted/capture_vid2 - yard sale 001.1.jpg";
+//			"S:/xfer/Vision testing/D035-v2 - car - plate.jpg";
+			"S:/Sessions/94-C6-91-18-C8-33/20190605-085938_capture_vid2/previous.jpg";
+	
 	
 	final static Display display = new Display();
 
@@ -39,9 +47,13 @@ public class VisionGUI {
 //	final private Composite compImageSource;
 //	final private Composite compImageAnalysis;
 	final private Composite compTextLog;
-	final private Composite compControls;
+//	final private Composite compControls;
 	final private Canvas canvasImageSource;
 	final private Canvas canvasImageAnalysis;
+	final private Text textReport;
+	final private StringBuilder sbReport = new StringBuilder();
+
+	private BatchAnnotateImagesResponse response;
 	
 	private Image imageSource;
 	private Image imageAnalysis;
@@ -71,8 +83,9 @@ public class VisionGUI {
 		this.canvasImageSource = new Canvas( compLeft, SWT.FILL );
 		this.canvasImageAnalysis = new Canvas( compLeft, SWT.FILL );
 		
+		this.textReport = new Text( compRight, SWT.MULTI );
 		this.compTextLog = new Composite( compRight, SWT.NONE );
-		this.compControls = new Composite( compRight, SWT.NONE );
+//		this.compControls = new Composite( compRight, SWT.NONE );
 		
 		this.shell.setText( VisionGUI.class.getName() );
 		
@@ -106,10 +119,12 @@ public class VisionGUI {
 		shell.setVisible( true );
 	}
 	
+	
 	public Shell getShell() {
 		return this.shell;
 	}
 
+	
 	public void setSourceImage( final String strImageFilename ) {
 		if ( null!=imageSource && !imageSource.isDisposed() ) {
 			imageSource.dispose();
@@ -120,127 +135,259 @@ public class VisionGUI {
 		this.shell.setRedraw( true );
 	}
 	
+
+	
+	public Image getAnalysisImage() {
+		return this.imageAnalysis;
+	}
+	
+	public String getAnalysisReport() {
+		return this.sbReport.toString();
+	}
+	
+	public void analyze( final Integer iWidth,
+						 final Integer iHeight ) {
+		if ( null==response ) return;
+		
+		if ( null!=imageAnalysis && ! imageAnalysis.isDisposed() ) {
+			imageAnalysis.dispose();
+		}
+		
+		if ( null!=iWidth && null!=iHeight ) {
+			imageAnalysis = new Image( display, iWidth, iHeight );
+		} else {
+			imageAnalysis = new Image( display, imageSource.getImageData() );
+		}
+		final GC gc = new GC( imageAnalysis );
+		
+		final Rectangle rect = gc.getClipping();
+		final ImageData id = imageSource.getImageData();
+		gc.drawImage( imageSource, 
+						0, 0, id.width, id.height, 
+						0, 0, rect.width, rect.height );
+		
+		sbReport.setLength( 0 );
+		
+//		System.out.println( "response count: " + response.getResponsesCount() );
+		
+		final AnnotateImageResponse air = response.getResponses( 0 );
+		
+		final List<LocalizedObjectAnnotation> 
+				listObjects = air.getLocalizedObjectAnnotationsList();
+		final List<EntityAnnotation> 
+				listText = air.getTextAnnotationsList();
+		final List<EntityAnnotation> 
+				listLabels = air.getLabelAnnotationsList();
+
+		
+
+		final Color colorObjects = display.getSystemColor( SWT.COLOR_GREEN );
+		final Color colorText = display.getSystemColor( SWT.COLOR_YELLOW );
+//		final Color colorShadow = display.getSystemColor( SWT.COLOR_DARK_GRAY );
+		final Color colorShadow = display.getSystemColor( SWT.COLOR_BLACK );
+
+		sbReport.append( "Label annotations:\n" );
+		
+		for ( final EntityAnnotation ea : listLabels ) {
+
+//			final BoundingPoly poly = ea.getBoundingPoly();
+//			drawPoly( e.gc, poly, ea.getDescription(), 
+//							id.width, id.height,
+//							rect.width, rect.height );
+			
+			final String strDescription = normalize( ea.getDescription() );
+			final float fScore = ea.getScore();
+//			final Map<FieldDescriptor, Object> map = ea.getAllFields();
+			
+			sbReport.append( String.format( 
+					"\tLabel (%.3f): %s", fScore, strDescription ) );
+
+			final List<LocationInfo> list = ea.getLocationsList();
+			if ( ! list.isEmpty() ) {
+				
+				sbReport.append( "  (LocationInfo in response)" );
+				
+//				for ( final LocationInfo li : list ) {
+//					final Map<FieldDescriptor, Object> mapFD = li.getAllFields();
+////					System.out.println( "map: " + mapFD );
+//				}
+			}
+			
+			sbReport.append( "\n" );
+		}
+
+
+		sbReport.append( "Localized Object annotations (shown in green):\n" );
+
+		System.out.println( "Drawing Localized Object annotations" );
+		for ( final LocalizedObjectAnnotation loa : listObjects ) {
+
+			final String strText = normalize( loa.getName() );
+			final float fScore = loa.getScore();
+
+			sbReport.append( String.format( 
+						"\tObject (%.3f): %s", fScore, strText ) );
+
+			final BoundingPoly poly = loa.getBoundingPoly();
+			final boolean bDrawn = drawPoly( 
+							gc, colorObjects, colorShadow,
+							poly, strText,
+							id.width, id.height,
+							rect.width, rect.height );
+			
+			if ( ! bDrawn ) {
+				sbReport.append( "  (not drawn)" );
+			}
+			sbReport.append( "\n" );
+		}
+		
+		
+		sbReport.append( "Text annotations (shown in yellow):\n" );
+
+		for ( final EntityAnnotation ea : listText ) {
+
+			final String strText = normalize( ea.getDescription() );
+//			final float fScore = ea.getScore();
+
+			sbReport.append( String.format( "\tText: %s", strText ) );
+
+			final BoundingPoly poly = ea.getBoundingPoly();
+			final boolean bDrawn = drawPoly( 
+							gc, colorText, colorShadow,
+							poly, strText, 
+							id.width, id.height,
+							rect.width, rect.height );
+			if ( ! bDrawn ) {
+				sbReport.append( "  (not drawn)" );
+			}
+			sbReport.append( "\n" );
+		}
+
+		final String strReport = sbReport.toString();
+		System.out.println( strReport );
+		final String strUIReport = 
+				StringUtils.replace( strReport, "\n", Text.DELIMITER );
+		textReport.setText( strUIReport );
+
+		
+	}
+	
+	
 	public void setAnalysisImage( final BatchAnnotateImagesResponse response ) {
 		if ( null==response ) return;
-		final Image image = this.imageSource;
+		this.response = response;
+		
+//		analyze();
+//		final PaintListener listener = createPaintListener( this.imageAnalysis );
+//		this.canvasImageAnalysis.addPaintListener( listener );
 		
 		this.canvasImageAnalysis.addPaintListener( new PaintListener() {
 			@Override
-			public void paintControl( final PaintEvent e ) {
-				final Rectangle rect = e.gc.getClipping();
-				final ImageData id = image.getImageData();
-				e.gc.drawImage( image, 
-								0, 0, id.width, id.height, 
-								0, 0, rect.width, rect.height );
-				final Color color = display.getSystemColor( SWT.COLOR_GREEN );
-				e.gc.setForeground( color );
+			public void paintControl( final PaintEvent event ) {
 				
-				System.out.println( "response count: " + response.getResponsesCount() );
+				analyze( event.width, event.height );
 				
-				final AnnotateImageResponse air = response.getResponses( 0 );
+				final Image image = imageAnalysis;
 				
-				final List<LocalizedObjectAnnotation> 
-						listObjects = air.getLocalizedObjectAnnotationsList();
-				final List<EntityAnnotation> 
-						listText = air.getTextAnnotationsList();
-				final List<EntityAnnotation> 
-						listLabels = air.getLabelAnnotationsList();
-
-				
-				
-				for ( final EntityAnnotation ea : listText ) {
-
-					System.out.println( "Drawing Text: " + ea.getDescription() );
-
-					final BoundingPoly poly = ea.getBoundingPoly();
-					drawPoly( e.gc, poly, ea.getDescription(), 
-									id.width, id.height,
-									rect.width, rect.height );
-
+				if ( null!=image && !image.isDisposed() ) {
+					final Rectangle rect = event.gc.getClipping();
+					final ImageData id = image.getImageData();
+					event.gc.drawImage( image, 
+									0, 0, id.width, id.height, 
+									0, 0, rect.width, rect.height );
 				}
-
-				for ( final LocalizedObjectAnnotation loa : listObjects ) {
-
-					System.out.println( "Drawing Label: " + loa.getName() );
-					
-					final BoundingPoly poly = loa.getBoundingPoly();
-					final boolean bPoly = drawPoly( 
-									e.gc, poly, loa.getName(),
-									id.width, id.height,
-									rect.width, rect.height );
-					
-					if ( !bPoly ) {
-//						e.gc.drawText( loa.getName(), 
-//								vFirst.getX(), vFirst.getY(), false );
-					}
-				}
-				
 			}
-		});
+			
+		} );
+
+	}
+	
+	
+	public static String normalize( final String strInput ) {
+		String strOutput = strInput.trim();
+		strOutput = StringUtils.replace( strOutput, "\n", " " );
+		return strOutput;
 	}
 
 	
 	public static boolean drawPoly( final GC gc,
+									final Color colorFore,
+									final Color colorShadow,
 								 	final BoundingPoly poly,
-								 	final String strName,
+								 	final String strText,
 								 	final int iImageWidth,
 								 	final int iImageHeight,
 								 	final int iWidth,
 								 	final int iHeight ) {
 		if ( null==poly ) return false;
+
+		final List<Point> listPoints = new LinkedList<>();
 		
 		final List<NormalizedVertex> listNorm = poly.getNormalizedVerticesList();
 		if ( null!=listNorm && !listNorm.isEmpty() ) {
 		
-//			final int iWidth = gc.getClipping().width;
-//			final int iHeight = gc.getClipping().height;
-			
-			final NormalizedVertex nvFirst = listNorm.get( 0 );
-			Point ptLast = null;
-			Point ptFirst = null;
 			for ( final NormalizedVertex nv : listNorm ) {
-				float fX = nv.getX();
-				float fY = nv.getY();
-				fX = fX * iWidth;
-				fY = fY * iHeight;
-				final int x = (int)fX;
-				final int y = (int)fY;
-				System.out.println( "\tfX: " + fX + ", fY:" + fY + "\tiX: " + x + ", iY:" + y );			
-				gc.drawOval( x-2, y-2, 4, 4 );
+				float fX = nv.getX() * iWidth;
+				float fY = nv.getY() * iHeight;
 				
-				if ( null!=ptLast ) {
-					gc.drawLine( x, y, ptLast.x, ptLast.y );
-				}
-				
-				ptLast = new Point( x, y );
-				if ( null==ptFirst ) ptFirst = ptLast;
+				final Point pt = new Point( (int)fX, (int)fY );
+				listPoints.add( pt );
 			}
-			gc.drawLine( ptFirst.x, ptFirst.y, ptLast.x, ptLast.y );
-
-			gc.drawText( strName, ptFirst.x + 2, ptFirst.y, true );
 		}
 		
 		final List<Vertex> listRaw = poly.getVerticesList();
 		if ( null!=listRaw && !listRaw.isEmpty() ) {
 			
+			for ( final Vertex vertex : listRaw ) {
+
+				final float fX = (float)vertex.getX() / iImageWidth * iWidth;
+				final float fY = (float)vertex.getY() / iImageHeight * iHeight;
+
+				final Point pt = new Point( (int)fX, (int)fY );
+				listPoints.add( pt );
+			}
+		}
+
+
+//		final String strNormText = strText.trim();
+
+		gc.setForeground( colorFore );
+
+		if ( ! listPoints.isEmpty() ) {
+			
 			Point ptLast = null;
 			Point ptFirst = null;
-			for ( final Vertex vertex : listRaw ) {
+			for ( final Point pt : listPoints ) {
 				
-				final int x = (int)((float)vertex.getX() / iImageWidth * iWidth);
-				final int y = (int)((float)vertex.getY() / iImageHeight * iHeight);
-	System.out.println( "\tX: " + x + ", Y:" + y );			
-				gc.drawOval( x-2, y-2, 4, 4 );
+//				gc.drawOval( pt.x-2, pt.y-2, 4, 4 );
 
 				if ( null!=ptLast ) {
-					gc.drawLine( x, y, ptLast.x, ptLast.y );
+					gc.drawLine( pt.x, pt.y, ptLast.x, ptLast.y );
 				}
 
-				ptLast = new Point( x, y );
-				if ( null==ptFirst ) ptFirst = ptLast;
+				ptLast = pt;
+				if ( null==ptFirst ) ptFirst = pt;
 			}
 			
-			gc.drawText( strName, ptFirst.x + 2, ptFirst.y, true );
+			final int iX = ptFirst.x;
+			final int iY = ptFirst.y;
+			gc.drawLine( iX, iY, ptLast.x, ptLast.y );
+
+			gc.setForeground( colorShadow );
+			gc.drawText( strText, iX + 1, iY - 1, true );
+			gc.drawText( strText, iX + 3, iY - 1, true );
+			gc.drawText( strText, iX + 1, iY + 1, true );
+			gc.drawText( strText, iX + 3, iY + 1, true );
+			gc.setForeground( colorFore );
+			gc.drawText( strText, iX + 2, iY, true );
+			
+//			System.out.println( "Drawing at " + iX + ", " + iY 
+//								+ " \"" + strText + "\"" );
+		} else {
+//			gc.drawText( "<no points> - " + strNormText, 4, 10, true );
+//			System.out.println( "No location identified for" 
+//								+ " \"" + strText + "\"" );
+			return false;
 		}
 		
 
@@ -264,6 +411,8 @@ public class VisionGUI {
 				
 				System.out.print( "Analyzing image..." );
 				vision.analyze();
+				System.out.print( "Examining..." );
+				gui.analyze( null, null );
 				System.out.println( "Done." );
 				
 				System.out.println( vision.getAnnotationResponse().toString() );
