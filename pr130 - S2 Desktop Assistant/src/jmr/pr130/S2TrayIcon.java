4e32dd5de63eba1f37a8e3ffd66d9ff1e7f60859
Zluz (C) <jeff.rabenhorst@gmail.com>
2019-06-22 17:33:34 -0400

    Improved taskbar icon

---
+++ pr130 - S2 Desktop Assistant/src/jmr/pr130/S2TrayIcon.java
@@ -0,0 +1,209 @@
+package jmr.pr130;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolTip;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+public class S2TrayIcon {
+
+	final static Display display;
+	final static Tray tray;
+	
+	static {
+		display = Display.getDefault();
+		tray = display.getSystemTray();
+	}
+	
+	final private Shell shell;
+	
+	private boolean bActive;
+	
+	
+//	final static String ICON_S2 = "/resources/icons/1872821.png"; // 3-line
+//	final static String ICON_S2 = "/resources/icons/121155.png"; // cyan
+	final static String ICON_S2 = "/resources/icons/116048.png"; // on-off
+
+	private TrayItem trayitem;
+	
+	
+	public static Image getS2Icon() {
+		final Class<?> classThis = S2TrayIcon.class;
+		final InputStream is = classThis.getResourceAsStream( ICON_S2 );
+		final Image image = new Image( display, is );
+		return image;
+	}
+	
+	
+	public S2TrayIcon() {
+		
+		bActive = true;
+		
+		this.trayitem = new TrayItem( tray, SWT.NONE );
+		trayitem.setToolTipText( "Desktop Assistant" );
+		
+		
+		final Image image = getS2Icon();
+		trayitem.setImage( image );
+		
+		this.shell = new Shell( display, SWT.NONE );
+		final Menu menu = new Menu( shell, SWT.POP_UP );
+		
+		final MenuItem miShowSchedule = new MenuItem( menu, SWT.PUSH );
+		final MenuItem miLaunchClient = new MenuItem( menu, SWT.PUSH );
+		new MenuItem( menu, SWT.SEPARATOR );
+		final MenuItem miShowMenu = new MenuItem( menu, SWT.RADIO );
+		final MenuItem miClipboard = new MenuItem( menu, SWT.RADIO );
+		final MenuItem miPostEvent = new MenuItem( menu, SWT.RADIO );
+		new MenuItem( menu, SWT.SEPARATOR );
+		final MenuItem miClose = new MenuItem( menu, SWT.PUSH );
+		
+		miShowSchedule.setText( "Show Scheduler UI" );
+		miLaunchClient.setText( "Launch latest S2 client" );
+		
+		miShowMenu.setText( "Show this pop-up menu" );
+		miClipboard.setText( "Clipboard to plain text" );
+		miPostEvent.setText( "Post S2 status event" );
+		
+		miShowMenu.setSelection( true );
+		
+		miClose.setText( "Close" );
+
+		trayitem.addListener( SWT.Selection, new Listener() {
+			@Override
+			public void handleEvent( final Event e ) {
+				if ( miShowMenu.getSelection() ) {
+					menu.setVisible( true );
+				} else if ( miClipboard.getSelection() ) {
+					doSetClipboardToPlainText();
+				} else if ( miPostEvent.getSelection() ) {
+					doPostStatusEvent();
+				}
+			}
+		});
+		
+		trayitem.addListener( SWT.MenuDetect, new Listener() {
+			@Override
+			public void handleEvent( final Event event ) {
+				menu.setVisible( true );
+			}
+		});
+		
+		miShowSchedule.addSelectionListener( new SelectionAdapter() {
+			@Override
+			public void widgetSelected( final SelectionEvent e ) {
+				SchedulerGUI.get().open();
+			}
+		});
+		
+		miClose.addSelectionListener( new SelectionAdapter() {
+			@Override
+			public void widgetSelected( final SelectionEvent e ) {
+				close();
+			}
+		});
+	}
+	
+	
+	public boolean isActive() {
+		return this.bActive;
+	}
+	
+	public void close() {
+		SchedulerGUI.get().close();
+		this.bActive = false;
+		this.trayitem.dispose();
+	}
+	
+	
+	public void doSetClipboardToPlainText() {
+		System.out.println( "--- doSetClipboardToPlainText()" );
+		
+		final Clipboard clipboard = new Clipboard( display );
+		final TextTransfer transfer = TextTransfer.getInstance();
+		final Object objContents = clipboard.getContents( transfer );
+		final String strContents = objContents.toString();
+		
+		System.out.println( "Setting clipboard to:\n"
+								+ "---start---\n" 
+								+ strContents + "\n"
+								+ "---end---" );
+		
+		clipboard.setContents( new String[] { strContents }, 
+								new Transfer[] { transfer } );
+		clipboard.dispose();
+		
+		showMessage( "Clipboard contents set to plain text", strContents );
+	}
+	
+	
+	public void doPostStatusEvent() {
+		System.out.println( "--- doPostStatusEvent()" );
+		showMessage( "Not supported", "Posting event not yet supported" );
+	}
+	
+	
+	public void showMessage( final String strTitle,
+							 final String strBody ) {
+		final int iStyle = SWT.BALLOON | SWT.ICON_INFORMATION;
+		final ToolTip tooltip = new ToolTip( this.shell, iStyle );
+
+		tooltip.setText( strTitle );
+		tooltip.setMessage( strBody );
+		tooltip.setAutoHide( false );
+		
+		this.trayitem.setToolTip( tooltip );
+		
+		tooltip.setVisible( true );
+		
+		final Thread thread = new Thread( "Delayed close of tooltip" ) {
+			public void run() {
+				try {
+					Thread.sleep( TimeUnit.SECONDS.toMillis( 4 ) );
+				} catch ( final InterruptedException e ) {}
+				display.asyncExec( new Runnable() {
+					@Override
+					public void run() {
+						tooltip.setVisible( false );
+						tooltip.dispose();
+					}
+				});
+			};
+		};
+		thread.start();
+	}
+	
+	
+	
+
+	public static void main( final String[] args ) {
+
+		final S2TrayIcon trayicon = new S2TrayIcon();
+
+	    while ( trayicon.isActive() && ! display.isDisposed() ) {
+		      if ( display.readAndDispatch()) {
+		    	  display.sleep();
+		      }
+		}
+	    if ( ! display.isDisposed() ) {
+	    	display.dispose();
+	    }
+	}
+	
+
+}
--- pr130 - S2 Desktop Assistant/src/jmr/pr130/SchedulerGUI.java
+++ pr130 - S2 Desktop Assistant/src/jmr/pr130/SchedulerGUI.java
@@ -1,38 +1,26 @@
 package jmr.pr130;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Tray;
-import org.eclipse.swt.widgets.TrayItem;
-
-import java.io.InputStream;
 
 public class SchedulerGUI {
 
-
-	final static Display display = new Display();
+//	final static Display display = Display.getCurrent();
+	final static Display display = S2TrayIcon.display;
 
 	final private Shell shell;
-	final static Tray tray = display.getSystemTray();
+	
+	private static SchedulerGUI instance = null;
 	
 	
-//	final static String ICON_S2 = "/resources/icons/1872821.png"; // 3-line
-//	final static String ICON_S2 = "/resources/icons/121155.png"; // cyan
-	final static String ICON_S2 = "/resources/icons/116048.png"; // on-off
-	
-	
-	public SchedulerGUI() {
-		shell = new Shell( display );
+	private SchedulerGUI() {
+		shell = new Shell( display, SWT.TITLE | SWT.MIN | SWT.RESIZE );
 		this.shell.setText( SchedulerGUI.class.getName() );
 		
 		shell.setLayout( new FillLayout() );
@@ -43,47 +31,24 @@ public class SchedulerGUI {
 		shell.setSize( 400, 200 );
 		shell.layout();
 		
-		final TrayItem ti = new TrayItem( tray, SWT.NONE );
-		ti.setToolTipText( "Desktop Assistant" );
-		
-		final Class<SchedulerGUI> classThis = SchedulerGUI.class;
-		final InputStream is = classThis.getResourceAsStream( ICON_S2 );
-		
-		final Image image = new Image( display, is );
-		ti.setImage( image );
+		final Image image = S2TrayIcon.getS2Icon();
 		shell.setImage( image );
 		
-		ti.addListener( SWT.Selection, new Listener() {
-			@Override
-			public void handleEvent( final Event e ) {
-				shell.setVisible( true );
-				shell.setActive();
-			}
-		});
-		
-		final Menu menu = new Menu( shell, SWT.POP_UP );
-		final MenuItem miShowSchedule = new MenuItem( menu, SWT.PUSH );
-		final MenuItem miClipboard = new MenuItem( menu, SWT.PUSH );
-		final MenuItem miPostEvent = new MenuItem( menu, SWT.PUSH );
-		
-		miShowSchedule.setText( "Show Scheduler GUI" );
-		miClipboard.setText( "Clipboard to plain text" );
-		miPostEvent.setText( "Post S2 status event" );
-		
-		ti.addListener( SWT.MenuDetect, new Listener() {
+		shell.addListener( SWT.Close, new Listener() {
 			@Override
 			public void handleEvent( final Event event ) {
-				menu.setVisible( true );
+				shell.setVisible( false );
+				event.doit = false;
 			}
 		});
-		
-		miShowSchedule.addSelectionListener( new SelectionAdapter() {
-			@Override
-			public void widgetSelected( final SelectionEvent e ) {
-				shell.setVisible( true );
-				shell.setActive();
-			}
-		});
+	}
+	
+	
+	public static synchronized SchedulerGUI get() {
+		if ( null==instance ) {
+			instance = new SchedulerGUI();
+		}
+		return instance;
 	}
 	
 
@@ -97,6 +62,11 @@ public class SchedulerGUI {
 		return this.shell;
 	}
 
+	
+	public void close() {
+		this.shell.close();
+		this.shell.dispose();
+	}
 	
 
 	public static void main( final String[] args ) {
