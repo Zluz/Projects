9b99ef34e71de0bfcc981a84c9bcca7eed9a29d3
Zluz <jeff.rabenhorst@gmail.com>
2020-06-24 11:37:26 -0400

    Processing script now shows summary (file count and elapsed time)

--- pr135 - ASN.1 Decoding/files/asn1tools_002/asn1tools_002.py
+++ pr135 - ASN.1 Decoding/files/asn1tools_002/asn1tools_002.py
@@ -5,11 +5,14 @@ import asn1tools
 import pickle
 from array import array
 # import json
+import datetime
 
+
+timeStart = datetime.datetime.now()
 
 print( "Arguments: ", str( sys.argv ) )
-for arg in sys.argv:
-  print( "\targ: ", arg )
+# for arg in sys.argv:
+#   print( "\targ: ", arg )
 
 # read schema text, generate binary
 
@@ -28,14 +31,16 @@ with open( 'Schema_LTE.pkl', 'rb' ) as input:
 
 # print ( pklLTE.decode( 'BCCH-DL-SCH-Message', bytearray.fromhex( '000E8409D590440590808FC208817A100802411016C1E00242348261892228880800' ) ) )
 
-# Binary input files named "LTE_*.bin" will be processed.
-# Output files will be created named "LTE_*.out".
+# Binary input files named "LTE_<id>.bin" will be processed.
+# Output files will be created named "LTE_<id>__<keyword>.out".
 
+iCount = 0
 
 dir = r'.'
 for entry in os.scandir( dir ):
   if ( entry.is_file() and entry.path.endswith( ".bin" ) ):
-    print( "\nProcessing: ", entry.path )
+    print( "\nProcessing:     ", entry.path )
+    iCount = iCount + 1
     
     size = os.path.getsize( entry.path )
 
@@ -50,32 +55,40 @@ for entry in os.scandir( dir ):
       for keyword in sys.argv:
         if not ".py" in keyword:
 
-          if "LTE_" in entry.path:
-            bytesJson = pklLTE.decode( keyword, arrayInput )
+          # bytesJson = ''
 
-          print( bytesJson )
-          
-          # print( bytearray( bytesJson ).decode() )
-          
-          strJson = str( bytesJson )
-          
-          filename = entry.path.rsplit('.bin')[0] + ".out"
-          print( filename )
-          
-          # file_out = open( filename, 'wt' )
-          # file_out.write( json )
-          # file_out.close()
+          try:
 
-#          with open( filename, 'w' ) as outfile:
-#            json.dump( [ bytesJson ], outfile )
-
-          file_out = open( filename, 'w' )
-#          file_out = open( filename, 'w+b' )
-#          binary = bytearray( bytesJson )
-          file_out.write( strJson )
-#          pickle.dump( file_out, bytesJson )
-          file_out.close()
-
-#          with open( filename, 'wb' ) as outfile:
-#            pickle.dump( bytesJson, outfile )
+            bytesJson = ''
           
+            if "LTE_" in entry.path:
+              bytesJson = pklLTE.decode( keyword, arrayInput )
+            # TODO add other formats here
+
+            strJson = str( bytesJson )
+            
+          except:
+            # print( "Exception encoutered: ", sys.exc_info()[0] )
+            strJson = ''
+
+
+          if ( strJson ):
+
+            # print( bytesJson )
+          
+            # print( bytearray( bytesJson ).decode() )
+          
+            filename = entry.path.rsplit('.bin')[0] + "__" + keyword + ".out"
+            
+            print( "Saving to file: ", filename )
+          
+            file_out = open( filename, 'w' )
+            file_out.write( strJson )
+            file_out.close()
+
+
+timeFinished = datetime.datetime.now()
+timeElapsed = timeFinished - timeStart
+iElapsedMS = int( timeElapsed.total_seconds() * 1000 )
+
+print( "\n", iCount, "file(s) processed in", iElapsedMS, "milliseconds." )
