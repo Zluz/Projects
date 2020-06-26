98085f5653b1c672686beb28fb620d557d76da19
Zluz <jeff.rabenhorst@gmail.com>
2020-06-26 09:57:04 -0400

    Cleaned up java demo program (removed comments, unused vars)

--- pr135 - ASN.1 Decoding/src/PyExecProcessManager.java
+++ pr135 - ASN.1 Decoding/src/PyExecProcessManager.java
@@ -9,32 +9,15 @@ import java.util.concurrent.TimeUnit;
 public class PyExecProcessManager {
 	
 
-	public final static String WORKING_DIR = 
-//			"C:\\Development\\CM\\Git_20190124.002\\Projects__20190125\\"
-//					+ "pr135 - ASN.1 Decoding\\files";
-			"C:\\Development\\CM\\Git_20190124.002\\Projects__20190125\\"
-					+ "pr135 - ASN.1 Decoding\\files\\"
-						+ "test_exe.006\\dist\\asn1tools_003";
+	public final static String WORKING_DIR = System.getProperty( "user.dir" ) 
+					+ "\\files\\test_exe.006\\dist\\asn1tools_003";
 	
-	public static final String PROG_PATH = 
-//			"C:\\Development\\CM\\Git_20190124.002\\Projects__20190125\\"
-//			+ "pr135 - ASN.1 Decoding\\files\\test_exe.005\\dist\\asn1tools_003";
-			WORKING_DIR;
-	
-//	public static final String PROG_FILE = "test_stdio.exe";
-//	public static final String PROG_FILE = "asn1tools_003.exe";
 	public static final String PROG_FILE = "asn1tools_003.exe";
-	
 	
 	public static final String CMD_PYTHON = 
 			"C:\\Development\\Runtimes\\Python 3.8.1 (32bit)\\python.exe";
 	
-
 	public final static String[] CMD_PARAMS = {
-			
-//			"/bin/sh",
-//			"/bin/bash",
-//			"/Local/scripts/exec_automationhat_input.sh",
 			
 //			CMD_PYTHON,
 //			"-u",
@@ -42,8 +25,7 @@ public class PyExecProcessManager {
 //			"C:\\Development\\CM\\Git_20190124.002\\Projects__20190125\\"
 //						+ "pr135 - ASN.1 Decoding\\files\\asn1tools_003.py",
 
-			PROG_PATH + "\\" + PROG_FILE
-						
+			WORKING_DIR + "\\" + PROG_FILE
 		};
 	
 	public final static String[] TIMED_INPUT = {
@@ -67,31 +49,6 @@ public class PyExecProcessManager {
 										final Process process ) {
 		final Thread threadStdOut = new Thread() {
 			
-			
-//			@Override
-//			public void run() {
-//				String strLine;
-//				try {
-//					final BufferedReader br = 
-//							new BufferedReader( new InputStreamReader( is ) );
-//					while (( strLine = br.readLine()) != null) {
-//						System.out.println( strLine );
-//					}
-//					br.close();
-//				} catch ( final Exception e ) {
-//					e.printStackTrace();
-//				}
-//			}
-			
-//			@Override
-//			public void run() {
-//				try {
-//					Streams.copy( process.getInputStream(), System.out );
-//				} catch ( final Exception e ) {
-//					e.printStackTrace();
-//				}
-//			}
-			
 			public void run() {
 				
 //				System.out.println( "Output monitor thread starting." );
@@ -99,18 +56,13 @@ public class PyExecProcessManager {
 				final InputStreamReader isr = new InputStreamReader( is );
 				try ( final BufferedReader br = new BufferedReader( isr ) ) {
 					
-//					System.out.println( "1" );
-					
 					String strLine = null;
 					while ( process.isAlive() ) {
 						while ( ( strLine = br.readLine() ) != null 
 													&& process.isAlive() ) {
-	//						System.out.println( "2" );
 							System.out.println( strPrefix + strLine );
 						}
-//						System.out.println( "3" );
 					}
-//					System.out.println( "4" );
 					
 				} catch ( final IOException e ) {
 					System.out.println( e.toString() + " encountered " );
@@ -128,14 +80,7 @@ public class PyExecProcessManager {
 	public static void main( final String[] args ) 
 					throws IOException, InterruptedException {
 		
-//		final File file = new File( PROG_PATH, PROG_FILE );
-//		final String strCommand = file.getAbsolutePath();
-//		final String strCommand = COMMAND;
-//		final String[] arrCommand = { strCommand };
 		final String[] arrCommand = CMD_PARAMS;
-		
-//		final ProcessBuilder pb = new ProcessBuilder( file.getAbsolutePath() );
-//		final Process process = pb.start();
 		
 		final File fileDir = new File( WORKING_DIR );
 		
@@ -145,7 +90,7 @@ public class PyExecProcessManager {
 		}
 		
 		final Process process = Runtime.getRuntime().exec( 
-								arrCommand, new String[]{}, fileDir );
+									arrCommand, new String[]{}, fileDir );
 		
 		final InputStream isOut = process.getInputStream();
 		final InputStream isErr = process.getErrorStream();
@@ -162,11 +107,6 @@ public class PyExecProcessManager {
 			TimeUnit.SECONDS.sleep( 1 );
 
 			final String strInput;
-//			if ( iCount < 20 ) {
-//				strInput = ""+ Math.random() + "";
-//			} else {
-//				strInput = "exit";
-//			}
 			
 			if ( iCount < TIMED_INPUT.length ) {
 				strInput = TIMED_INPUT[ iCount ];
@@ -189,65 +129,5 @@ public class PyExecProcessManager {
 		final int iExit = process.exitValue();
 		System.out.println( "Process ended, exit code: " + iExit );
 	}
-
-
-	
-//	public static void main_( final String[] args ) 
-//					throws IOException, InterruptedException {
-//		
-//		final File file = new File( PROG_PATH, PROG_FILE );
-//		
-////		final String strCommand = COMMAND;
-//		
-////		final String strCommand = file.getAbsolutePath();
-////		final String[] arrCommand = { strCommand };
-//		final String[] arrCommand = CMD_PARAMS;
-//		
-////		System.out.println( "Starting: " + strCommand );
-//		
-//		final MonitorProcess monitor = 
-//						new MonitorProcess( "test", arrCommand, true );
-//
-//		monitor.start();
-//
-//		monitor.addListener( new MonitorProcess.Listener() {
-//			@Override
-//			public void process( 	final long lTime, 
-//									final String strLine ) {
-//				System.out.println( "out> " + strLine );
-//			}
-//		});
-//
-//
-//		int iCount = 0;
-//		
-//		while ( monitor.isRunning() ) {
-//			iCount++;
-////			System.out.println( "Still running" );
-//			TimeUnit.SECONDS.sleep( 1 );
-//		}
-//	}
-
-	
-//	public static void main( final String[] args ) 
-//					throws IOException, InterruptedException {
-//		
-//		final File file = new File( PROG_PATH, PROG_FILE );
-//		
-//		
-//		final String strCommand = file.getAbsolutePath();
-//		final String[] arrCommand = { strCommand };
-//		
-//		System.out.println( "Starting: " + strCommand );
-//		
-//		final RunProcess rp = new RunProcess( arrCommand );
-//		rp.run();
-//		
-//		while ( true ) {
-//			System.out.println( rp.getOutputLine() );
-//			TimeUnit.SECONDS.sleep( 1 );
-//		}
-//	}
-
 	
 }
