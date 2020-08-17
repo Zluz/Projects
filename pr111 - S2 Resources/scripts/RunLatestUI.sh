c5bafd9bd9f0803b0c6d5b6aba9b10bc8e3cb5b3
Zluz <jeff.rabenhorst@gmail.com>
2020-08-17 12:54:09 -0400

    Added share-mounted test to RunLatestUI.sh

--- pr111 - S2 Resources/scripts/RunLatestUI.sh
+++ pr111 - S2 Resources/scripts/RunLatestUI.sh
@@ -13,7 +13,29 @@ echo "Writing log: ${FILE_LOG}"
 `sleep 1;/Local/scripts/conky.sh > /dev/null 2>&1;` &
 
 export rundir=/Share/Development/Export
+
+if [[ -e $rundir ]]
+then
+	echo "Share is mounted, Export directory found."
+else
+	echo -n "Share does not appear to be available. Waiting (up to 60 seconds) .. "
+	for i in 1 2 3 4 5 6
+	do
+		if [[ -e $rundir ]]
+		then
+			// share is mounted .. just iterate through loop ..
+		else
+			echo -n "$i.. "
+			sleep 10
+		fi
+	done
+fi
+
 export latest=`ls -1t $rundir/pr101_* | head -1`
+
+echo "Running: $latest"
+echo ""
+
 cd $rundir
 /usr/bin/java -jar $latest | tee $FILE_LOG
 
