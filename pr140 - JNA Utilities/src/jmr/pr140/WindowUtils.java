b77973319f39c1ac35470a800c7b58a137572cb8
jmraben <jmraben>
2020-10-14 18:06:31 -0400

    Copy WIP

---
+++ pr140 - JNA Utilities/src/jmr/pr140/WindowUtils.java
@@ -0,0 +1,91 @@
+package jmr.pr140;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Tlhelp32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinDef.RECT;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+import com.sun.jna.platform.win32.WinUser.WINDOWINFO;
+import com.sun.jna.win32.StdCallLibrary;
+
+import jmr.pr140.ProcessUtils.Kernel32;
+
+public class WindowUtils {
+
+	public static class WindowInfo {
+		
+		public final String strTitle;
+		public final HWND hwnd;
+		public final int iStyle;
+		public final RECT rectWindow;
+		
+		public WindowInfo( 	final String strTitle,
+							final HWND hwnd,
+							final int iStyle,
+							final RECT rectWindow ) {
+			this.strTitle = strTitle;
+			this.hwnd = hwnd;
+			this.iStyle = iStyle;
+			this.rectWindow = rectWindow;
+		}
+	}
+	
+	
+	
+	
+	public static HWND getForegroundWindow() {
+		final HWND hwnd = User32.INSTANCE.GetForegroundWindow();
+		return hwnd;
+	}
+	
+	public static String getWindowTitle( final HWND hwnd ) {
+		if ( null==hwnd ) return null;
+		char[] buffer = new char[ 2048 ];
+		User32.INSTANCE.GetWindowText( hwnd, buffer, 2048 );
+		final String strText = Native.toString( buffer );
+		return strText;
+	}
+	
+	
+	public static WindowInfo getWindowInfo( final HWND hwnd ) {
+		final String strTitle = getWindowTitle( hwnd );
+		
+		final WINDOWINFO pwi = new WINDOWINFO();
+		User32.INSTANCE.GetWindowInfo( hwnd,  pwi );
+		//TODO lots more info in here, something may be useful
+		final RECT rect = pwi.rcWindow;
+		final int iStyle = pwi.dwStyle;
+		
+		final WindowInfo wi = new WindowInfo( strTitle, hwnd, iStyle, rect );
+		return wi;
+	}
+	
+	
+	// see SO SO35393786
+	public interface User32_SO35393786 extends StdCallLibrary {
+		User32_SO35393786 INSTANCE = 
+				(User32_SO35393786)Native.loadLibrary( 
+						"user32", User32_SO35393786.class );
+		
+	}
+	
+	
+	
+	public static Map<HWND,String> getAllWindows() {
+		final Map<HWND,String> map = new HashMap<>();
+//		USER32 ...
+		return map;
+	}
+	
+	
+	
+	public static void main( final String[] args ) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
